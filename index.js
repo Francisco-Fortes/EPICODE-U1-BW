fb00182ecb318dfb0456a14dc62633606307873e
Daniel <dan.earp@icloud.com>
2022-10-18 11:17:56 +0100

    Checkbox Logic
    Added logic to ensure the checkbox is checked before being able to proceed

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-/**
- * Updates the donut chart's percent number and the CSS positioning of the progress bar.
- * Also allows you to set if it is a donut or pie chart
- * @param  {string}  el      The selector for the donut to update. '#thing'
- * @param  {number}  percent Passing in 22.3 will make the chart show 22%
- * @param  {boolean} donut   True shows donut, false shows pie
- */
-function updateDonutChart(el, percent, donut) {
-  percent = Math.round(percent)
-  if (percent > 100) {
-    percent = 100
-  } else if (percent < 0) {
-    percent = 0
-  }
-  var deg = Math.round(360 * (percent / 100))
-
-  if (percent > 50) {
-    $(el + ' .pie').css('clip', 'rect(auto, auto, auto, auto)')
-    $(el + ' .right-side').css('transform', 'rotate(180deg)')
+const isCheckboxTicked = function () {
+  if (document.getElementById('consent-checkbox').checked) {
+    alert('Your ready to take the exam!')
   } else {
-    $(el + ' .pie').css('clip', 'rect(0, 1em, 1em, 0.5em)')
-    $(el + ' .right-side').css('transform', 'rotate(0deg)')
+    alert(
+      'You must agree to answer the questions by yourself before proceeding',
+    )
   }
-  if (donut) {
-    $(el + ' .right-side').css('border-width', '0.1em')
-    $(el + ' .left-side').css('border-width', '0.1em')
-    $(el + ' .shadow').css('border-width', '0.1em')
-  } else {
-    $(el + ' .right-side').css('border-width', '0.5em')
-    $(el + ' .left-side').css('border-width', '0.5em')
-    $(el + ' .shadow').css('border-width', '0.5em')
-  }
-  $(el + ' .num').text(percent)
-  $(el + ' .left-side').css('transform', 'rotate(' + deg + 'deg)')
 }
-
-// Pass in a number for the percent
-updateDonutChart('#specificChart', 66.67, true)
