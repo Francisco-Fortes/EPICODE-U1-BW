1dccb3c6880c50acde86571edd8e2b4f4d3a9de5
SandroIerna <iernasandro@gmail.com>
2022-10-18 15:20:09 +0200

    adds timer function

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-/**
- * Updates the donut chart's percent number and the CSS positioning of the progress bar.
- * Also allows you to set if it is a donut or pie chart
- * @param  {string}  el      The selector for the donut to update. '#thing'
- * @param  {number}  percent Passing in 22.3 will make the chart show 22%
- * @param  {boolean} donut   True shows donut, false shows pie
- */
-function updateDonutChart(el, percent, donut) {
-  percent = Math.round(percent)
-  if (percent > 100) {
-    percent = 100
-  } else if (percent < 0) {
-    percent = 0
-  }
-  var deg = Math.round(360 * (percent / 100))
+// Timer------------------------------------------------------------
 
-  if (percent > 50) {
-    $(el + ' .pie').css('clip', 'rect(auto, auto, auto, auto)')
-    $(el + ' .right-side').css('transform', 'rotate(180deg)')
+let difficulty = 60;
+let time = difficulty + 1;
+let timeInDonut = document.getElementById("seconds");
+
+setInterval(updateCountdown, 1000);
+function updateCountdown() {
+  // console.log(seconds);
+  time--;
+  if (time > 0) {
+    timeInDonut.innerText = time;
   } else {
-    $(el + ' .pie').css('clip', 'rect(0, 1em, 1em, 0.5em)')
-    $(el + ' .right-side').css('transform', 'rotate(0deg)')
+    timeInDonut.innerText = "TimeOut!";
   }
-  if (donut) {
-    $(el + ' .right-side').css('border-width', '0.1em')
-    $(el + ' .left-side').css('border-width', '0.1em')
-    $(el + ' .shadow').css('border-width', '0.1em')
-  } else {
-    $(el + ' .right-side').css('border-width', '0.5em')
-    $(el + ' .left-side').css('border-width', '0.5em')
-    $(el + ' .shadow').css('border-width', '0.5em')
-  }
-  $(el + ' .num').text(percent)
-  $(el + ' .left-side').css('transform', 'rotate(' + deg + 'deg)')
 }
-
-// Pass in a number for the percent
-updateDonutChart('#specificChart', 66.67, true)
+console.log(time);
