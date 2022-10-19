85cb8bb71fd55e84a34f893abbc3e92031675ccb
Francisco-Fortes <fortesfco@gmail.com>
2022-10-19 09:34:21 +0200

    Third commit

--- index.js
+++ index.js
@@ -5,34 +5,80 @@
  * @param  {number}  percent Passing in 22.3 will make the chart show 22%
  * @param  {boolean} donut   True shows donut, false shows pie
  */
-function updateDonutChart(el, percent, donut) {
-  percent = Math.round(percent)
-  if (percent > 100) {
-    percent = 100
-  } else if (percent < 0) {
-    percent = 0
-  }
-  var deg = Math.round(360 * (percent / 100))
 
-  if (percent > 50) {
-    $(el + ' .pie').css('clip', 'rect(auto, auto, auto, auto)')
-    $(el + ' .right-side').css('transform', 'rotate(180deg)')
-  } else {
-    $(el + ' .pie').css('clip', 'rect(0, 1em, 1em, 0.5em)')
-    $(el + ' .right-side').css('transform', 'rotate(0deg)')
-  }
-  if (donut) {
-    $(el + ' .right-side').css('border-width', '0.1em')
-    $(el + ' .left-side').css('border-width', '0.1em')
-    $(el + ' .shadow').css('border-width', '0.1em')
-  } else {
-    $(el + ' .right-side').css('border-width', '0.5em')
-    $(el + ' .left-side').css('border-width', '0.5em')
-    $(el + ' .shadow').css('border-width', '0.5em')
-  }
-  $(el + ' .num').text(percent)
-  $(el + ' .left-side').css('transform', 'rotate(' + deg + 'deg)')
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+];
+
+let score = 0;
+const buttonsContainer = document.getElementById("buttons-container");
+const nodeNextButton = document.getElementById("next-button-container");
+const questionCounter = document.getElementById("question-counter");
+const quizzQuestions = [];
+const generateQuestion = function () {
+  let listOfQuestions = questions[2];
+  displayQuestion.innerText = listOfQuestions.question;
+};
+
+const arrayMergedAnswers = [];
+let incorrect_answers = questions[1].incorrect_answers;
+let correct_answer = questions[1].correct_answer;
+for (i = 0; i < incorrect_answers.length; i++) {
+  arrayMergedAnswers.push(incorrect_answers[i]);
 }
+arrayMergedAnswers.push(correct_answer);
 
-// Pass in a number for the percent
-updateDonutChart('#specificChart', 66.67, true)
+console.log(arrayMergedAnswers[i]);
+const generateAnswers = function () {
+  for (i = 0; i < arrayMergedAnswers.length; i++) {
+    const allButtons = document.createElement("button");
+    buttonsContainer.appendChild(allButtons);
+    allButtons.innerText = arrayMergedAnswers[i];
+    allButtons.classList.add("button-class");
+    allButtons.setAttribute("onclick", "clickAnswers(event)");
+  }
+};
+
+const clickAnswers = function (event) {
+  if (event.target.innerText === correct_answer) {
+    score++;
+  } else {
+    console.log("incorrect");
+  }
+};
+
+const generateNextButton = function () {
+  const nextButton = document.createElement("button");
+  nodeNextButton.appendChild(nextButton);
+  nextButton.innerText = "Next";
+  nextButton.classList.add("next-button-class");
+  nextButton.setAttribute("onclick", "clickNext(event)");
+};
